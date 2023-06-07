7080737c36dc63c3831811fe3f0fb5401b95da49
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-06-07 10:42:24 -0500

    laid out the logic for my server.js file

--- server.js
+++ server.js
@@ -1,5 +1,60 @@
-//* GIVEN a command-line application that accepts user input
-//* WHEN I start the application
-//* THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+const inquirer = require("inquirer");
+const department = require("./Develop/lib/department");
+const employee = require("./Develop/lib/employee");
+const role = require("./Develop/lib/role");
 
-// TODO: Create a start() application function that presents user with options to: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role.
+//* Function to start the application
+function startApp() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "Add a department",
+          "View all employees",
+          "Add an employee",
+          "Update an employee role",
+          "View all roles",
+          "Add a role",
+          "Exit",
+        ],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.action) {
+        case "View all departments":
+          department.viewAllDepartments();
+          break;
+        case "Add a department":
+          department.addDepartment();
+          break;
+        case "View all employees":
+          employee.viewAllEmployees();
+          break;
+        case "Add an employee":
+          employee.addEmployee();
+          break;
+        case "Update an employee role":
+          employee.updateEmployeeRole();
+          break;
+        case "View all roles":
+          role.viewAllRoles();
+          break;
+        case "Add a role":
+          role.addRole();
+          break;
+        case "Exit":
+          console.log("Goodbye!");
+          process.exit(0);
+      }
+    })
+    .catch((err) => {
+      console.error("Error occurred:", err);
+    });
+}
+
+// Start the application
+startApp();
