21a3335dc9e4736b6df048767999aa3e9c162338
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-06-07 13:29:35 -0500

    refactoring code

--- server.js
+++ server.js
@@ -1,60 +1,71 @@
+require("dotenv").config();
+const {
+  buildConnectionOptions,
+  createConnection,
+} = require("./config/dbConfig");
 const inquirer = require("inquirer");
 const department = require("./Develop/lib/department");
 const employee = require("./Develop/lib/employee");
 const role = require("./Develop/lib/role");
 
 //* Function to start the application
-function startApp() {
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "action",
-        message: "What would you like to do?",
-        choices: [
-          "View all departments",
-          "Add a department",
-          "View all employees",
-          "Add an employee",
-          "Update an employee role",
-          "View all roles",
-          "Add a role",
-          "Exit",
-        ],
-      },
-    ])
-    .then((answer) => {
-      switch (answer.action) {
-        case "View all departments":
-          department.viewAllDepartments();
-          break;
-        case "Add a department":
-          department.addDepartment();
-          break;
-        case "View all employees":
-          employee.viewAllEmployees();
-          break;
-        case "Add an employee":
-          employee.addEmployee();
-          break;
-        case "Update an employee role":
-          employee.updateEmployeeRole();
-          break;
-        case "View all roles":
-          role.viewAllRoles();
-          break;
-        case "Add a role":
-          role.addRole();
-          break;
-        case "Exit":
-          console.log("Goodbye!");
-          process.exit(0);
-      }
-    })
-    .catch((err) => {
-      console.error("Error occurred:", err);
-    });
+async function startApp() {
+  const connection = await createConnection(buildConnectionOptions());
+
+  const { createPromptModule } = inquirer;
+  const prompt = createPromptModule();
+
+  const selectedOption = await prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "Add a department",
+        "View all employees",
+        "Add an employee",
+        "Update an employee role",
+        "View all roles",
+        "Add a role",
+        "Exit",
+      ],
+    },
+  ]);
+
+  try {
+    switch (selectedOption.action) {
+      case "View all departments":
+        department.viewAllDepartments(connection);
+        break;
+      case "Add a department":
+        department.addDepartment(connection);
+        break;
+      case "View all employees":
+        employee.viewAllEmployees(connection);
+        break;
+      case "Add an employee":
+        employee.addEmployee(connection);
+        break;
+      case "Update an employee role":
+        employee.updateEmployeeRole(connection);
+        break;
+      case "View all roles":
+        role.viewAllRoles(connection);
+        break;
+      case "Add a role":
+        role.addRole(connection);
+        break;
+      case "Exit":
+        console.log("Goodbye!");
+        process.exit(0);
+    }
+  } catch (error) {
+    console.error("Error occurred:", error);
+  } finally {
+    // Close the database connection
+    connection.end();
+  }
 }
 
-// Start the application
 startApp();
